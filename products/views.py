c7c929995347190dd9186f6d80fa2e035c6f64fc
JValenueva <rykerluno@gmail.com>
2023-02-27 11:00:49 -0700

    created the functionality to update product in the products app view file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -5,7 +6,7 @@ from .serializers import ProductSerializer
 from .models import Product
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET', 'POST'])
 def products_list(request):
 
     if request.method == 'GET':
@@ -18,19 +19,15 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-    elif request.method == 'PUT':
-        pass
-    elif request.method == 'DELETE':
-        pass
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def product_detail(request,pk):
-    try:
-        product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
         serializer = ProductSerializer(product);
         return Response(serializer.data)
-    
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND);
-    
-    
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
